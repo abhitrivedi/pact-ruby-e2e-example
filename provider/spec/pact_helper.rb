7eaa33a933a7f2ef1edf4679656ec61adacdc267
Abhi Trivedi <atrivedi@mdsol.com>
2018-10-19 16:47:55 -0700

    adding global and base states

--- provider/spec/pact_helper.rb
+++ provider/spec/pact_helper.rb
@@ -11,3 +11,22 @@ Pact.service_provider "Bar" do
     pact_uri './consumer/spec/support/foo-bar.json'
   end
 end
+
+# When conusmer interaction has no provider state then these global and base states do not get executed.
+Pact.provider_states_for 'Foo' do
+  set_up do
+    p 'Inside base provider set_up for Foo'
+  end
+
+  tear_down do
+    p 'Inside base provider tear_down for Foo'
+  end
+end
+
+Pact.set_up do
+  p 'Inside global provider set_up'
+end
+
+Pact.tear_down do
+  p 'Inside global tear_down'
+end
